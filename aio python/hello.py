192369dab7bb24b6f3414c5f28b66221fe93ebfe
njmikell0226 <njmikell0226@gmail.com>
2021-04-05 22:35:48 -0400

    commit

--- aio python/hello.py
+++ aio python/hello.py
@@ -1,1 +1,17 @@
-print("Hello, World")
+# This is a python comment
+
+# This variable contains an interger.
+
+quantity = 2
+
+# This variable contains a float.
+
+unit_price = 1.99
+
+# This variable contains the result of multiplying quantity times unit price
+
+extended_price = quantity * unit_price
+
+# Show the results
+
+print(extended_price)
