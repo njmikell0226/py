0a14fee0568c6429ef121d5f9fa663886af850d8
Mike Little <njmikell0226@gmail.com>
2022-01-08 21:26:15 -0500

    test

--- 4hr youtube/funtions.py
+++ 4hr youtube/funtions.py
@@ -5,5 +5,5 @@ def say_hi(name, age):
 #print("Top") - will run function commands from top to bottom.
 say_hi("Mike", "35")
 say_hi("Steve", "70")
-say_hi("Mikaelah", "10")
+say_hi("Mikaelah", "11")
 #print("Bottom")
