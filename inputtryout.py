fde53bb2d1a7beb45684b738981201e5ef6780bb
MIke Little <njmikell0226@gmail.com>
2022-03-02 19:39:45 -0500

    pyfiles

---
+++ inputtryout.py
@@ -0,0 +1,12 @@
+
+print('This is going to be cool')
+user_name = input ('What is your name?:')
+
+print (user_name,', Okay cool.')
+
+print('One more question, how old are you?:')
+age = input ('Enter your age: ')
+
+print (user_name, 'is ', age, "years old! Okay now!! Final Question")
+print('hello')
+final_answer = input('What is your problem?! Why did the FOOD take forever!!! Im dead. TY')
