15e0395fc9f046a4609ed299cf2956e4d5cb14c8
MIke Little <njmikell0226@gmail.com>
2022-03-20 20:12:07 -0400

    snippits

---
+++ python_snippits.py
@@ -0,0 +1,29 @@
+# Comparisons:
+# Equal:               ==
+# Not Equal:           !=
+# Greater Than:        >
+# Less Than:           <
+# Greater or Equal:    >=
+# Less or Equal:       <=
+# Object Identity:     is
+
+# False Values:
+    # False
+    # None
+    # Zero of any numeric type
+    # Any empty sequence. For example, '', (), [].
+    # Any empty mapping. For example, {}.
+
+# List []
+# String ('')
+# Dictionary {}
+
+# print (id(a)) - look up
+
+
+# condition = None
+
+# if condition:
+#     print('Evaluation to True')
+# else:
+#     print('Evaluation to False')
